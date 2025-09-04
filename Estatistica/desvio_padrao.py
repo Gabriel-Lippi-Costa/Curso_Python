e48502e86a46e67bc3533c33c38a843afcfda696
Gabriel-Lippi-Costa <gabriellippidacosta@gmail.com>
2025-09-04 10:08:39 -0300

    Algoritmo de desvio padrão está pronto

---
+++ Estatistica/desvio_padrao.py
@@ -0,0 +1,36 @@
+import statistics
+import math
+
+conjunto_valores = [41, 38, 39, 45, 47, 41, 44, 41, 37, 42]
+lista_desvios = []
+lista_desvios_quadrados = []
+
+def calcular_media_conjunto_valores(conjunto_valores):
+    media_conjunto_valores = statistics.mean(conjunto_valores)
+    return media_conjunto_valores
+
+def calcular_variancia_populacional(conjunto_valores, media_conjunto_valores):
+    soma_desvio = 0
+    soma_desvio_quadrado = 0
+
+    for valor in conjunto_valores:
+        desvio = valor - media_conjunto_valores
+        soma_desvio += desvio
+        desvio_quadrado = (valor - media_conjunto_valores) ** 2
+        soma_desvio_quadrado += desvio ** 2
+        lista_desvios.append(desvio)
+        lista_desvios_quadrados.append(desvio_quadrado)
+
+    return soma_desvio, soma_desvio_quadrado, lista_desvios, lista_desvios_quadrados
+
+resultado_media_conjunto = calcular_media_conjunto_valores(conjunto_valores)
+
+resultado_desvio, resultado_desvio_quadrado, lista_desvios, lista_desvios_quadrados = calcular_variancia_populacional(conjunto_valores, resultado_media_conjunto)
+
+print(f"Essa é a sua média do conjunto: {resultado_media_conjunto}\n")
+print(f"Essa é a sua lista de desvios: {lista_desvios}")
+print(f"Esse é o seu total de desvios: {resultado_desvio}\n")
+print(f"Esse é o seu total do desvios ao quadrado: {resultado_desvio_quadrado}")
+print(f"Essa é a sua lista de desvios ao quadrado: {lista_desvios_quadrados}")
+print(f"Essa é a sua variância populacional: {resultado_desvio_quadrado / len(conjunto_valores)}")
+print(f"Esse é o seu desvio padrão: {round(math.sqrt(resultado_desvio_quadrado / len(conjunto_valores)))}")
