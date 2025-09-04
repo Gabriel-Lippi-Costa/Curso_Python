0a9d8152673dc150a32fa9b83b11c51e5e025006
Gabriel-Lippi-Costa <gabriellippidacosta@gmail.com>
2025-09-04 11:28:53 -0300

    Começando o algoritmo de variância amostral

---
+++ Estatistica/variancia_amostral.py
@@ -0,0 +1,14 @@
+import statistics
+import math
+
+conjunto_valores = [4, 7, 6, 7, 9, 5, 8, 10, 9, 8, 7, 10]
+lista_desvios = []
+lista_desvios_quadrados = []
+
+def calcular_media_conjunto_valores():
+    pass
+
+def calcular_variancia_amostral():
+    pass
+
+resultado_media_conjunto = calcular_media_conjunto_valores(conjunto_valores)
